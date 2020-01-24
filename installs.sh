e762aa68af578741ecacf3e929fae42024efcdaa
Guy Wilson <guy@draftbuff.com>
2020-01-24 02:12:59 -0800

    alias adds and npm

--- installs.sh
+++ installs.sh
@@ -4,11 +4,20 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
 apt update
 apt upgrade -y
 apt install -y build-essential curl file git python-pip python3-pip python3.8 python3.8-pip python3.8-venv \
- docker-ce docker-ce-cli containerd.io snapd awscli
+ docker-ce docker-ce-cli containerd.io snapd awscli npm
 snap install pycharm-professional --classic
+npm upgrade npm
 pip install --upgrade pip
 pip3 install --upgrade pip
 python3.8 -m pip --upgrade pip
 
+alias update='apt update && apt upgrade -y'
+alias install='apt install'
+alias upgrade='apt upgrade'
+alias autoremove='apt autoremove'
+alias purge='apt purge'
+alias remove='apt remove'
+alias pipremove='python3.8 -m pip uninstall'
+alias pipinstall='python3.8 -m pip install'
 # apt install mysql-server
 # apt install mysql-workbench
--- local_installs.sh
+++ local_installs.sh
@@ -1,3 +1,7 @@
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 brew tap aws/tap
 brew install aws-sam-cli
+npm install -g aws-cdk
+alias update='apt update && apt upgrade -y'
+
+# aws configure
