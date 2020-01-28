e6053913c47ea619785f9395a8c2d2a1c395b774
Guy Wilson <guy@draftbuff.com>
2020-01-28 00:22:53 -0800

    Update more local install commands

--- installs.sh
+++ installs.sh
@@ -1,23 +1,14 @@
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
-apt update
-apt upgrade -y
-apt install -y build-essential curl file git python-pip python3-pip python3.8 python3.8-pip python3.8-venv \
- docker-ce docker-ce-cli containerd.io snapd awscli npm
-snap install pycharm-professional --classic
-npm upgrade npm
-pip install --upgrade pip
-pip3 install --upgrade pip
-python3.8 -m pip --upgrade pip
-
 alias update='apt update && apt upgrade -y'
 alias install='apt install'
 alias upgrade='apt upgrade'
 alias autoremove='apt autoremove'
 alias purge='apt purge'
 alias remove='apt remove'
-alias pipremove='python3.8 -m pip uninstall'
-alias pipinstall='python3.8 -m pip install'
-# apt install mysql-server
-# apt install mysql-workbench
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
+update
+install -y build-essential curl file git python-pip python3-pip python3.8 python3.8-pip python3.8-venv \
+ docker-ce docker-ce-cli containerd.io snapd awscli npm google-chrome-stable
+service docker start
+apt install mysql-server
--- local_installs.sh
+++ local_installs.sh
@@ -1,7 +1,16 @@
+snap install node --channel=13 --classic
+snap install pycharm-professional --classic
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 brew tap aws/tap
 brew install aws-sam-cli
-npm install -g aws-cdk
-alias update='apt update && apt upgrade -y'
-
 # aws configure
+npm upgrade -g
+npm install -g aws-cdk
+pip install --upgrade pip
+pip3 install --upgrade pip
+python3.8 -m pip --upgrade pip
