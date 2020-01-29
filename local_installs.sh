6b77b9cff2476e88bd3664a3f2f02971b1aa8024
Guy Wilson <guywilsonjr@gmail.com>
2020-01-29 02:35:25 -0800

    more changes and additions

--- local_installs.sh
+++ local_installs.sh
@@ -1,16 +1,21 @@
-snap install node --channel=13 --classic
-snap install pycharm-professional --classic
+#wget https://github.com/jakeday/linux-surface/releases/download/5.1.15-1/linux-headers-5.1.15-surface-linux-surface_5.1.15-surface-linux-surface-8_amd64.deb
+#wget https://github.com/jakeday/linux-surface/releases/download/5.1.15-1/linux-image-5.1.15-surface-linux-surface_5.1.15-surface-linux-surface-8_amd64.deb
+#wget https://github.com/jakeday/linux-surface/releases/download/5.1.15-1/linux-libc-dev_5.1.15-surface-linux-surface-8_amd64.deb
+#sudo dpkg -i linux*.deb
+sudo snap install node --channel=13 --classic
+sudo snap install pycharm-professional --classic
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+brew install gcc
 brew tap aws/tap
 brew install aws-sam-cli
 # aws configure
-npm upgrade -g
-npm install -g aws-cdk
+sudo npm upgrade -g
+sudo npm install -g aws-cdk
 pip install --upgrade pip
 pip3 install --upgrade pip
 python3.8 -m pip --upgrade pip
