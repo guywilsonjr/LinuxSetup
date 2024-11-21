e9c72f101bb98dc2566bdc2c26905254f07cc580
Guy Wilson <guywilsonjr@gmail.com>
2024-11-21 12:15:57 -0800

    Add python opt script git config script

---
+++ git_configs.sh
@@ -0,0 +1,6 @@
+git config --global checkout.workers 16
+git config --global checkout.thresholdForParallelism 400
+git config --global fetch.parallel 8
+git config --global http.maxRequests 20
+git config --global submodule.fetchJobs 4
+#fetch.unpackLimit 500
